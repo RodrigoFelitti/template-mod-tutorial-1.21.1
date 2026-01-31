243cfde3f6e666b2692c4d8b45effebf7a08a9d6
RodrigoFelitti <rodrigofelitti@gmail.com>
2026-01-31 01:35:11 -0300

    initial commit

---
+++ src/main/java/net/feneko/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.feneko.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
